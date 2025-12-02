a8e7de1103a8b8550f02170ed08405e4f3ac0a98
Robson F. Vilela <robsonfvilela@MacBook-Pro-de-Robson.local>
2025-12-02 08:56:01 -0300

    no message for this commit

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,13 @@ commit="no message for this commit"
 [[ "${1}" ]] && commit=${1}
 
 cd public
+git checkout main
 git add .
 git commit -m "${commit}"
 git push origin main
 
 cd ..
+git checkout main
 git add .
 git commit -m "${commit}"
 git push origin main
